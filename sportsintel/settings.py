b7ae379401ee44c75ede5b0e28e424216e49e933
Stanley Okafor <ezebo001@gmail.com>
2022-01-17 16:38:28 +0100

    Django

--- sportsintel/settings.py
+++ sportsintel/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from waitress import serve
-from my_wsgi_project import app
+
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
